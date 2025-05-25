e175b05be400f480902bb27181fde795cb03b89c
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2025-05-26 00:00:08 +0600

    banner, slider added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../pages/Home/Shared/Footer/Footer";
-import Navbar from "../pages/Home/Shared/Navbar/Navbar";
+import Navbar from "../pages/Shared/Navbar/Navbar";
+import Footer from "../pages/Shared/Footer/Footer";
 
 const Main = () => {
   return (
---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,42 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "./Category.css";
+
+import slide1 from "../../../assets/slide1.jpg";
+import slide2 from "../../../assets/slide2.jpg";
+import slide3 from "../../../assets/slide3.jpg";
+import slide4 from "../../../assets/slide4.jpg";
+
+const Category = () => {
+  return (
+    <>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper mb-24"
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default Category;

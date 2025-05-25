5f2c7b3ec56e55df8afb4175565372c7ecce55ea
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2025-05-25 15:37:24 +0600

    routes and layout setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div className="max-w-screen-xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
+  </React.StrictMode>
+);
